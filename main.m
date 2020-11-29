f54e51587bc51730670e06e12d295f959b151c96
BatCamus <batcamus@gmail.com>
2020-11-29 12:06:17 +0100

    V2
    Corection de la création de la matrice qui était fausse

--- main.m
+++ main.m
@@ -10,7 +10,7 @@ Rint=30;
 Rpi=48;
 Rext=75;
 Qint=(101.3e3)^2;
-Qpi=(200e3)^2;
+Qpi=(400e3)^2;
 Qext=(101.3e3)^2;
 dx=0.1;
 N=(Rpi-Rint)/dx+1;
@@ -19,23 +19,25 @@ x=Rint:dx:Rext;
 
 %% Système matriciel
 
-A1=zeros(N); A2=zeros(M);  B1=zeros(N,1); B2=zeros(M,1);
+A1=zeros(N); B1=zeros(N,1); A2=zeros(M); B2=zeros(M,1);
+
 for i=2:N-1
-    A1(i,i)=-((x(i)+dx/2)+(x(i)-dx/2))/dx^2;
+    A1(i,i)=-2*((x(i)+dx/2)+(x(i)-dx/2))/dx^2;
     A1(i,i-1)=(x(i)-dx/2)/dx^2;
     A1(i,i+1)=(x(i)+dx/2)/dx^2;
     B1(i)=0;
 end
-for j = N+1 : M-1
-    A2(j,j)=-((x(j)+dx/2)+(x(j)-dx/2))/dx^2;
+for j = 2 : M-1
+    A2(j,j)=-2*((x(j)+dx/2)+(x(j)-dx/2))/dx^2;
     A2(j,j-1)=(x(j)-dx/2)/dx^2;
     A2(j,j+1)=(x(j)+dx/2)/dx^2;
     B2(j)=0;
 end
+
 A1(1,1)=1;B1(1)=Qint;
 A1(N,N)=1;B1(N)=Qpi;
 A2(1,1)=1;B2(1)=Qpi;
-A2(N,N)=1;B2(M)=Qext;
+A2(M,M)=1;B2(M)=Qext;
 
 
 Q1=A1\B1;
